e323ca4e946aac7fa2a99f3096227372e89934a8
jh1004ben <88893644+wngud1004@users.noreply.github.com>
2023-01-02 23:30:59 +0900

    Create form.php

---
+++ Login Form 만들기/form.php
@@ -0,0 +1,19 @@
+<?php
+# form 데이터 가져오기
+$uid = $_GET['id'];
+$name = $_GET['name'];
+$pwd = $_GET['pwd'];
+$point = $_GET['point'];
+$regdate = $_GET['regdate'];
+
+var_dump($uid);
+echo "<br>";
+var_dump($name);
+echo "<br>";
+var_dump($pwd);
+echo "<br>";
+var_dump($point);
+echo "<br>";
+var_dump($regdate);
+echo "<br>";
+?>
